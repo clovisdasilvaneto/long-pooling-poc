8992ab944223c11a5fd0aeec61fc5ff7cf5207fd
Clóvis Neto <clovisdasilvaneto@gmail.com>
2023-09-11 11:38:16 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+let express = require('express');
+let cors = require('cors')
+let app = express()
+let bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+const store = {
+}
+
+let subscribers = {};
+
+function onSubscribe(req, res) {
+  res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+  res.setHeader("Cache-Control", "no-cache, must-revalidate");
+  
+  const {id, initial} = req.query;
+  
+  if(initial && Object.keys(store).length) return res.json(store);
+
+  subscribers[id] = res;
+
+    console.log('## subscribing to id', id)
+
+    console.log('--- subscribers:', Object.keys(subscribers))
+
+  console.log(req.query)
+
+  req.on('close', function() {
+    console.log('### closing request for id', id)
+    delete subscribers[id];
+  });
+}
+
+function publish() {
+  
+  for (let id in subscribers) {
+    let res = subscribers[id];
+    res.json(store);
+  }
+
+  subscribers = {};
+}
+
+app.get('/subscribe', (req, res) => {
+    onSubscribe(req, res);
+})
+
+app.post('/publish', (req, res) => {
+    // accept POST
+    req.setEncoding('utf8');
+    
+    const { key, content } = req.body;
+    store[key] = content
+    
+    publish();
+    res.end("ok");
+
+    return;
+})
+
+app.listen(8080, function () {
+    console.log('web server listening on port 8080')
+})
